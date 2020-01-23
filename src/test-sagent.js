b674035f54f6cd759e931ece3900cc3c35d45205
Yo shinzato <s19008@std.it-college.ac.jp>
2020-01-23 16:31:59 +0900

    first commit

---
+++ src/test-sagent.js
@@ -0,0 +1,8 @@
+const sagent = require('superagent')
+const URI = 'http://localhost:3000/fruits.json'
+
+const callback = (err, res) => 
+    console.log(res.body)
+
+sagent.get(URI)
+   .end(callback)
